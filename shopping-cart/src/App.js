98534166e45bb841550bc5c66e3c8b71f2cd72e3
Rupali Singh <rupalisingh2304@gmail.com>
2022-04-14 21:55:54 +0530

    Add shopping cart data with THUNK!!\

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,14 +1,31 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { sendCartData } from "./slices/cart-slice";
 import { Provider } from "react-redux";
 import store from "./store/index";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
+let isInitial = true;
 function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartisVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <Layout>
-        <Cart />
+        {showCart && <Cart />}
         <Products />
       </Layout>
     </Provider>
--- shopping-cart/src/slices/cart-slice.js
+++ shopping-cart/src/slices/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const initialState = { items: [], totalQunatity: 0 };
 
@@ -8,37 +9,73 @@ const cartSlice = createSlice({
   reducers: {
     addItemtoCart(state, action) {
       const newItem = action.payload;
-      const existingItems = state.items.find(item => item.id === newItem.id)
-      state.totalQunatity++
-      if(!existingItems){
-          state.items.push({
-              id : newItem.id,
-              quantity : 1,
-              price : newItem.price,
-              totalPrice : newItem.price,
-              name : newItem.title
-
-          })
-      }else{
-          existingItems.quantity++
-          existingItems.totalPrice += existingItems.price
+      const existingItems = state.items.find((item) => item.id === newItem.id);
+      state.totalQunatity++;
+      if (!existingItems) {
+        state.items.push({
+          id: newItem.id,
+          quantity: 1,
+          price: newItem.price,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItems.quantity++;
+        existingItems.totalPrice += existingItems.price;
       }
     },
     removeItemtoCart(state, action) {
-        const id = action.payload
-        const ExistingItem = state.items.find(item => item.id === id)
-        state.totalQunatity--
-        if(ExistingItem.quantity === 1){
-            state.items = state.items.filter(item => item.id !== id)
-        }else{
-            ExistingItem.quantity--
-        }
+      const id = action.payload;
+      const ExistingItem = state.items.find((item) => item.id === id);
+      state.totalQunatity--;
+      if (ExistingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        ExistingItem.quantity--;
+      }
     },
   },
 });
 
-export const cartSliceReducer = cartSlice.reducer
-export const cartSliceActions = cartSlice.actions
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "sending...",
+        message: "sending cart data",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch("sample.com", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
 
-export default cartSlice
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!!",
+          message: "SEnd cart data successfully",
+        })
+      );
+    } catch (error) {
+      uiActions.showNotification({
+        status: "error",
+        title: "Error!!",
+        message: "Sending Cart Data Failed!",
+      })
+    }
+  };
+};
 
+export const cartSliceReducer = cartSlice.reducer;
+export const cartSliceActions = cartSlice.actions;
+
+export default cartSlice;
